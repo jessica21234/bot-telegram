e9d953a85ba926b7239aadd8274f79fa1b6ff940
jessica21234 <fullatlerdezz@gmail.com>
2026-06-06 13:28:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN        = os.environ.get("TOKEN")
 PAYPAL_EMAIL = os.environ.get("PAYPAL_EMAIL")
-CANAL_NOM = os.environ.get("CANAL_NOM")
-ADMIN1 = os.environ.get("ADMIN1", "@irk14")
-ADMIN2 = os.environ.get("ADMIN2", "@ilyan_dugafe")
-ADMIN3 = os.environ.get("ADMIN3")
+CANAL_NOM    = os.environ.get("CANAL_NOM")
+ADMIN1       = os.environ.get("ADMIN1", "@irk14")
+ADMIN2       = os.environ.get("ADMIN2", "@ilyan_dugafe")
+ADMIN3       = os.environ.get("ADMIN3")
+CHANNEL_ID   = os.environ.get("CHANNEL_ID")
